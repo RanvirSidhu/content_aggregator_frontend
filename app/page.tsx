4ad92c83d32e4c29148212ccad983ba4c4dba550
Ranvir Singh Sidhu <70250483+RanvirSidhu@users.noreply.github.com>
2026-01-28 22:32:34 +0530

    Build was failing due to the use of useSearchParams in component requires suspense tags around the return tags.

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,12 @@
-"use client";
 
-import { useEffect, useState } from 'react';
-import { Header } from './components/Header';
-import { FilterPanel } from './components/FilterPanel';
-import { ErrorAlert } from './components/ErrorAlert';
-import { ArticlesList } from './components/ArticlesList';
-import { useArticles } from './hooks/useArticles';
-import Pagination from './components/Pagination';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+import { Suspense } from 'react';
+import Home from './components/Home';
 
-export default function Home() {
-  const searchParams = useSearchParams()
-  const pathname = usePathname();
-  const { replace } = useRouter();
 
-  const urlPageNo = searchParams.get("page");
-  const urlSource = searchParams.get("source");
-  const urlSearchTitle = searchParams.get("search_title");
-
-  const [selectedSource, setSelectedSource] = useState(urlSource ? urlSource : "all");
-  const [currentPage, setPageChange] = useState(urlPageNo ? +urlPageNo : 1);
-  const [searchTitle, setSearchTitle] = useState<string | undefined>(urlSearchTitle ? urlSearchTitle : undefined);
-
-  const {
-    articles,
-    sources,
-    loading,
-    error,
-    lastUpdate,
-    totalEntries,
-    autoRefresh,
-    toggleAutoRefresh,
-    refresh,
-  } = useArticles(selectedSource, currentPage, searchTitle);
-
-  const changeSourceFilter = (source: string) => {
-    setPageChange(1);
-    setSelectedSource(source);
-    const params = new URLSearchParams(searchParams);
-    if (source && source != "all") {
-      params.set('source', source);
-    } else {
-      params.delete('source');
-    }
-    params.delete('page');
-    replace(`${pathname}?${params.toString()}`);
-  };
-
-  const changePage = (newPage: number) => {
-    setPageChange(newPage)
-    const params = new URLSearchParams(searchParams);
-    if (newPage && newPage != 1) {
-      params.set('page', newPage.toString());
-    } else {
-      params.delete('page');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }
-
-  const changeSearchTitle = useDebouncedCallback((title: string) => {
-    setPageChange(1);
-    console.log(title);
-    setSearchTitle(title);
-
-    const params = new URLSearchParams(searchParams);
-    if (title) {
-      params.set('search_title', title);
-    } else {
-      params.delete('search_title');
-    }
-    params.delete('page');
-    replace(`${pathname}?${params.toString()}`);
-  }, 300);
-
+export default function Page() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
-      <div className="max-w-7xl mx-auto p-6">
-        <Header
-          loading={loading}
-          lastUpdate={lastUpdate}
-          articlesCount={totalEntries}
-          autoRefresh={autoRefresh}
-          onRefresh={refresh}
-          onAutoRefreshChange={toggleAutoRefresh}
-        />
-
-        <FilterPanel
-          sources={sources}
-          selectedSource={selectedSource}
-          placeholder={searchTitle}
-          onSourceChange={changeSourceFilter}
-          onSearchChange={changeSearchTitle}
-        />
-
-        {error && <ErrorAlert error={error} />}
-        <div className='bg-white rounded-lg shadow-md pt-6 px-6 mb-6'>
-          <ArticlesList articles={articles} loading={loading} />
-          <Pagination currentPage={currentPage} totalEntries={totalEntries} onPageChange={changePage} />
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </Suspense>
   );
 }
